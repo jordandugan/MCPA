2414117b2af332e6748d061456fd5e1341bb2cc8
Jordan Dugan <jordandugan@AD.DOE.CARLETON.CA>
2019-01-23 16:56:05 -0500

    speed plot

--- MonteCarlo1D.m
+++ MonteCarlo1D.m
@@ -10,16 +10,21 @@ xp = x - v*dt;
 xpp = x -2*v*dt;
 for i = 1:2000
     if rand() <= 0.05 
-        v = 0;
+        v(i) = 0;
     else 
         x = -xpp + 2*xp + a*dt^2;
-        v = (x - xpp)/(2*dt);
+        v(i) = (x - xpp)/(2*dt);
     end
     t = t+dt;
     vdrift(i) = x/t;
+    subplot(2,1,1)
     plot(x,0, 'o', 'MarkerSize', 10);
     xlim([0, 1e-23]);
     title(sprintf('%e m/s', vdrift(i)));
+    subplot(2,1,2)
+    plot(v)
+    title('speed');
+    ylim([0, 1e-10]);
     drawnow
     xpp = xp;
     xp = x;
--- MultiplteParticleMonteCarlo.m
+++ MultiplteParticleMonteCarlo.m
@@ -1,5 +1,5 @@
 x = 1e-23*rand(5,1);
-v = zeros(5,1);
+v = 0;
 t = 0;
 x0 = x;
 dt = 1e-15;
@@ -10,15 +10,20 @@ xp = x - v*dt;
 xpp = x -2*v*dt;
 for i = 1:2000
     xscat = rand(5,1) <= 0.05;
-    v(xscat) = 0;
+    v(i,xscat) = 0;
     x(~xscat) = -xpp(~xscat) + 2*xp(~xscat) + a*dt^2;
-    v(~xscat) = (x(~xscat) - xpp(~xscat))/(2*dt);
+    v(i,~xscat) = (x(~xscat) - xpp(~xscat))/(2*dt);
     
     t = t+dt;
     vdrift(i) = mean(x-x0)/t;
+    subplot(2,1,1)
     plot(x,0, 'o', 'MarkerSize', 10);
     xlim([0, 1e-23]);
     title(sprintf('%e m/s', vdrift(i)));
+    subplot(2,1,2)
+    plot(v)
+    title('speed')
+    ylim([0, 1e-10]);
     drawnow
     xpp = xp;
     xp = x;
